7fc45be7d2f97be21fc205f4d202f3dfe4be50db
Muhammed Fawzy <52621707+MuhammedFawzy020@users.noreply.github.com>
2023-01-23 18:50:56 +0200

    First Backend Comite

---
+++ resources/views/backend/cars/index.blade.php
@@ -0,0 +1,31 @@
+@extends('backend.partial.layout')
+@section('content')
+<section class="content">
+   <div class="container-fluide">
+      <div class="row">
+          <div class="col-lg-12">
+              <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">All Cars</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                 
+                  </thead>
+                  <tbody>
+              
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+
+             </div>
+             </div>
+             </div>
+             </div>
+</section>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route::get('/', function () {
 Route::get('/compare', function () {
     return view('frontend.pages.compare');
 });
+
+
+
+//Cars Routes
+Route::get('/dashboard/car-index', 'App\Http\Controllers\CarsController@index')->name('car-index');
+Route::get('/dashboard/car-create', 'App\Http\Controllers\CarsController@create')->name('car-create');
+Route::post('/dashboard/car-store', 'App\Http\Controllers\CarsController@store')->name('car-store');
+Route::get('/dashboard/car-edit/{id}', 'App\Http\Controllers\CarsController@edit')->name('car-edit');
+Route::post('/dashboard/car-update', 'App\Http\Controllers\CarsController@update')->name('car-update');
+Route::post('/dashboard/car-delete/{id}', 'App\Http\Controllers\CarsController@destroy')->name('car-delete');
