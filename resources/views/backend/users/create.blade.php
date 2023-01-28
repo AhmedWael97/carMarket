6ff9658e38d0ab2f016fe922a2941b8e35ef35b2
Muhammed Fawzy <52621707+MuhammedFawzy020@users.noreply.github.com>
2023-01-28 15:01:24 +0200

    all changes in user views

--- resources/views/backend/users/create.blade.php
+++ resources/views/backend/users/create.blade.php
@@ -14,7 +14,7 @@
               <div class="card-body">
                 <form action="{{ route('user-update') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                    
+                
                     <div class="row">
                         <div class="col-md-6">
                             <label>
